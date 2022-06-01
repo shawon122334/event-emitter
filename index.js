ec4195bc2d61abc183d1d4f04baaa6e37220596f
sabbir <shawon.mu11@gmail.com>
2022-06-02 02:33:55 +0600

    created event emitter and learn how to use it

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
 // event emitter
 // we need event module 
 const EventEmitter = require("node:events") // so that it does not contradict with third party module
+
+// lets implement in node
+
+const myEmitter = new EventEmitter(); // created an object using EventEmitter class
+console.log(myEmitter); // we will see how many events we created , events count , max listeners 
+
+// event register/listener using the object we created 
+myEmitter.on('event',()=>{
+    console.log("an event is triggered");
+})
+
+// another listener for the event 
+myEmitter.on('event',()=>{
+    console.log("Another listener")
+})
+// trigger or emit the event 
+myEmitter.emit('event')
+
+//----------------------------------------------------------------------
+// advantage 
+// we have to define all the events in call back , but we can define many listeners individually using event emitter 
+//----------------------------------------------------------------------
+
+
+myEmitter.on('one',function(a,b){
+    console.log(a,b);
+    console.log(this); // to see object property (myEmitter object)
+})
+myEmitter.emit('one','Hello','World')
+
+//------------------------------------------------------------------------
+// now if we think that EventEmitter class is not enough for our need , we can add more functionality to it. lets see how : 
+class WithLog extends EventEmitter{
+    execute(callback){
+        console.log('Before Execution');
+        this.emit('begin')
+        callback();
+        this.emit('end')
+        console.log('After Execution');
+
+    }
+}
+// lets create an object for WithLog class 
+const withLog = new WithLog();
+withLog.on('end',()=> console.log('End execution') )
+withLog.on('begin',()=> console.log('Begin execution') )
+withLog.execute(()=> console.log('--executing task--') );
+//-------------------------------------------------------------------------
